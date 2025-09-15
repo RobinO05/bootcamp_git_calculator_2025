80f15a80dfb2e3e8a59fcde8887c7506371736e9
09robin05 <robin.oste@otto.de>
2025-09-15 15:10:03 +0200

    main menu

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
+running = True
 def main():
-    x = 2.0
-    y = 4.0
+    x = 0
+    y = 0
+    while running:
+        print("Welcome to your Calculator!")
+        print("Main Menu"
+              "Addition - 1"
+              "Subtraktion - 2"
+              "Multiplikation - 3"
+              "Division - 4"
+              "Quit Calculator - 0")
+        choosed_option = int(input("Please choose an option: "))
+        if choosed_option == 1:
+            x = int(input("Please enter the first number"))
+            y = int(input("Please enter the second number"))
+            add(x, y)
+        if choosed_option == 2:
+            x = int(input("Please enter the first number"))
+            y = int(input("Please enter the second number"))
+            sub(x, y)
+        if choosed_option == 3:
+            x = int(input("Please enter the first number"))
+            y = int(input("Please enter the second number"))
+            mult(x, y)
+        if choosed_option == 4:
+            x = int(input("Please enter the first number"))
+            y = int(input("Please enter the second number"))
+            div(x, y)
+        if choosed_option == 0:
+            running = False
 
-    print(f"{x} + {y} = {add(x, y)}")
+
 
 
 def add(x, y):
