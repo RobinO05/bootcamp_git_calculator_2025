5a1eb95a5d74285cb18cda585d5f9ea75d6b1e3c
09robin05 <robin.oste@otto.de>
2025-09-15 15:24:03 +0200

    hh

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import time
+
+
 def main():
     running = True
     x = 0
@@ -17,26 +20,32 @@ def main():
             x = int(input("Please enter the first number"))
             y = int(input("Please enter the second number"))
             add(x, y)
+            time.sleep(2)
         if choosed_option == 2:
             x = int(input("Please enter the first number"))
             y = int(input("Please enter the second number"))
             sub(x, y)
+            time.sleep(2)
         if choosed_option == 3:
             x = int(input("Please enter the first number"))
             y = int(input("Please enter the second number"))
             mult(x, y)
+            time.sleep(2)
         if choosed_option == 4:
             x = int(input("Please enter the first number"))
             y = int(input("Please enter the second number"))
             div(x, y)
+            time.sleep(2)
         if choosed_option == 5:
             x = int(input("Please enter the first number"))
             y = int(input("Please enter the second number"))
             mod(x, y)
+            time.sleep(2)
         if choosed_option == 6:
             x = int(input("Please enter the first number"))
             y = int(input("Please enter the second number"))
             power(x, y)
+            time.sleep(2)
         if choosed_option == 0:
             running = False
 
